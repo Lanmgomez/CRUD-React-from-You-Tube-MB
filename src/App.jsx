f79cca93abffcbdcba131c1be84385f698f20810
islan <islan_gomes@hotmail.com>
2023-01-05 10:17:05 -0300

    atualização manhã 10:16 (05/01/2023)

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <BrowserRouter>
       <MenuNavbar />
-        <Container>
+        <Container customClass="min-height">
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/AboutUs" element={<AboutUs />}/>
--- src/layout/Container.jsx
+++ src/layout/Container.jsx
@@ -1,8 +1,8 @@
-import "./Container.sass"
+import styles from "./Container.module.css"
 
 const Container = (props) => {
   return (
-    <div className="container">{props.children}</div>
+    <div className={`${styles.container} ${styles[props.customClass]}`}>{props.children}</div>
   )
 }
 
