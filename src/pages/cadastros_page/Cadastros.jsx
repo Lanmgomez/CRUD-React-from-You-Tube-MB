735697d6c3d0a6c91224688547531e11026d20c6
islan <islan_gomes@hotmail.com>
2023-01-05 14:05:15 -0300

    atualização 14h:05min  05/01/2023

--- src/pages/cadastros_page/Cadastros.jsx
+++ src/pages/cadastros_page/Cadastros.jsx
@@ -1,16 +1,29 @@
-import Messages from "../../layout/Messages"
+// css
 import "./Cadastros.sass"
+// hooks
 import { useLocation } from "react-router-dom"
+import { useState, useEffect } from "react" 
+// components
+import Messages from "../../layout/Messages"
 import Container from "../../layout/Container"
 import NewProjectButton from "../../components/Button/NewProjectButton"
+import ProjectCardInfos from "../../components/Projects_Cards_Infos/ProjectCardInfos"
  
 const Cadastros = () => {
 
+  const [registeredProjects, setRegisteredProjects] = useState([])
+  
   const location = useLocation()
   let message = ""
   if (location.state) {
     message = location.state.message
   }
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/projects/")
+         .then((response) => { setRegisteredProjects(response.data) })
+         .catch((error) => console.log(error))
+  }, [])
 
   return (
     <div className="project-container">
@@ -19,8 +32,17 @@ const Cadastros = () => {
         <NewProjectButton to={`/NewProject`} text="Novo Projeto" />
       </div>
       {message && <Messages msg={message} type={`success`} />}
-      <Container>
-          <p>Projetos...</p>
+      <Container customClass="start">
+          {registeredProjects.length > 0 && 
+            registeredProjects.map((registers) => (
+              <ProjectCardInfos 
+                  key={registers.id}
+                  name={registers.name} 
+                  budget={registers.budget}
+                  category={registers.category.name}
+              />
+            ))}
+            {registeredProjects.length == "" && <p>Não há projetos cadastrados...</p>}
       </Container>
     </div>
   )
