596ea1d77692b3905eee53e95b005d7662d70f0b
islan <islan_gomes@hotmail.com>
2023-01-06 11:23:52 -0300

    atualizações (06-01-2023) 11h:23min

---
+++ src/components/Projects_Cards_Infos/ProjectCardInfos.jsx
@@ -0,0 +1,32 @@
+// css
+import styles from "./ProjectCardInfos.module.css"
+// react icons
+import { BsPencil, BsFillTrashFill } from "react-icons/bs"
+// hooks
+import { Link } from 'react-router-dom'
+
+const ProjectCardInfos = ({ id, name, budget, category, handleRemove }) => {
+
+  const removeProject = (e) => {
+    e.preventDefault()
+    handleRemove(id)
+  }
+
+  return (
+    <div className={styles.register_card}>
+        <h4>{name}</h4>
+            <p><span>Orçamento:</span> R${budget}</p>
+            <p className={styles.category_text}><span className={`${styles[category.toLowerCase()]}`}></span>{category}</p>
+        <div className={styles.project_card_actions}>
+            <Link>
+                <BsPencil /> Editar
+            </Link>
+            <button onClick={removeProject}>
+                <BsFillTrashFill /> Excluir    
+            </button>    
+        </div>    
+    </div>
+  )
+}
+
+export default ProjectCardInfos
--- src/pages/cadastros_page/Cadastros.jsx
+++ src/pages/cadastros_page/Cadastros.jsx
@@ -8,10 +8,13 @@ import Messages from "../../layout/Messages"
 import Container from "../../layout/Container"
 import NewProjectButton from "../../components/Button/NewProjectButton"
 import ProjectCardInfos from "../../components/Projects_Cards_Infos/ProjectCardInfos"
+import Loader from "../../components/Loader/Loader"
  
 const Cadastros = () => {
 
   const [registeredProjects, setRegisteredProjects] = useState([])
+  const [loadingImg, setLoadingImg] = useState(false)
+  const [sucessDeletedMessage, setSucessDeletedMessage] = useState(false)
   
   const location = useLocation()
   let message = ""
@@ -20,10 +23,26 @@ const Cadastros = () => {
   }
 
   useEffect(() => {
-    axios.get("http://localhost:5000/projects/")
-         .then((response) => { setRegisteredProjects(response.data) })
+    setTimeout(() => {
+      axios.get("http://localhost:5000/projects/")
+         .then((response) => { setRegisteredProjects(response.data), setLoadingImg(true) })
          .catch((error) => console.log(error))
+    }, 300)
   }, [])
+
+  const DeleteMessage = () => {
+    const timer = setSucessDeletedMessage(true)
+    setTimeout(() => { setSucessDeletedMessage(false) }, 3000)
+    return () => clearTimeout(timer)
+  }
+
+  const handleRemoveProject = (id) => {
+    axios.delete(`http://localhost:5000/projects/${id}`)
+         .then(setRegisteredProjects(registeredProjects.filter((register) => register.id !== id)),
+               DeleteMessage()
+         )         
+         .catch((error) => console.log(error)) 
+  }
 
   return (
     <div className="project-container">
@@ -37,12 +56,16 @@ const Cadastros = () => {
             registeredProjects.map((registers) => (
               <ProjectCardInfos 
                   key={registers.id}
+                  id={registers.id}
                   name={registers.name} 
                   budget={registers.budget}
                   category={registers.category.name}
+                  handleRemove={handleRemoveProject}
               />
             ))}
             {registeredProjects.length == "" && <p>Não há projetos cadastrados...</p>}
+            {!loadingImg && <Loader />}
+            {sucessDeletedMessage && <p className="delete-msg">Projeto deletado com sucesso!</p>}
       </Container>
     </div>
   )
